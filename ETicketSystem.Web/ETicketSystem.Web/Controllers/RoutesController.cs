ef2e79493d976a84577fa1eb4129d9ece3ac6de8
u-ashikov <u.ashikov@abv.bg>
2017-12-08 01:54:12 +0200

    Search routes form, home page container background, css

---
+++ ETicketSystem.Web/ETicketSystem.Web/Controllers/RoutesController.cs
@@ -0,0 +1,12 @@
+﻿namespace ETicketSystem.Web.Controllers
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	public class RoutesController : BaseController
+    {
+
+    }
+}
---
+++ ETicketSystem.Web/ETicketSystem.Web/Models/Routes/SearchRouteFormModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ETicketSystem.Web.Models.Routes
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+
+	public class SearchRouteFormModel
+    {
+		public int StartTown { get; set; }
+
+		public int EndTown { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime Date { get; set; }
+    }
+}
