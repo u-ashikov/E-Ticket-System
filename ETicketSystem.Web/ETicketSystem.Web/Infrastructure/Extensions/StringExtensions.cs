c5715473f9aaa3b1b635f8339c1d011d3ad93b27
u-ashikov <u.ashikov@abv.bg>
2017-12-10 20:12:32 +0200

    String extension transform to friendly url

--- ETicketSystem.Web/ETicketSystem.Web/Infrastructure/Extensions/StringExtensions.cs
+++ ETicketSystem.Web/ETicketSystem.Web/Infrastructure/Extensions/StringExtensions.cs
@@ -1,10 +1,13 @@
 ﻿namespace ETicketSystem.Web.Infrastructure.Extensions
 {
+	using System.Text.RegularExpressions;
+
 	public static class StringExtensions
     {
-		public static string ToFormatedPrice(this decimal price)
-		{
-			return price.ToString("0.00");
-		}
-    }
+		public static string ToFormatedPrice(this decimal price) =>
+			price.ToString("0.00");
+
+		public static string ToFriendlyUrl(this string text)
+			=> Regex.Replace(text, @"[^A-Za-z0-9_\.~]+", "-").ToLower();
+	}
 }
