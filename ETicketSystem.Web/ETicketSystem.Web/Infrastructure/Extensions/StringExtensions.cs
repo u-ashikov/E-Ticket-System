babd6be32859af6760f45ade28d295504b62bd6f
u-ashikov <u.ashikov@abv.bg>
2017-12-10 19:53:11 +0200

    String extensions

---
+++ ETicketSystem.Web/ETicketSystem.Web/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ETicketSystem.Web.Infrastructure.Extensions
+{
+	public static class StringExtensions
+    {
+		public static string ToFormatedPrice(this decimal price)
+		{
+			return price.ToString("0.00");
+		}
+    }
+}
