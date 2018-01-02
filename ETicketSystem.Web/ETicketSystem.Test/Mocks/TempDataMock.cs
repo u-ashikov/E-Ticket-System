fcae77ae0c6f52bc658789aac6af264deebbf6d5
u-ashikov <u.ashikov@abv.bg>
2018-01-02 16:11:36 +0200

    Temp data mock

---
+++ ETicketSystem.Web/ETicketSystem.Test/Mocks/TempDataMock.cs
@@ -0,0 +1,11 @@
+﻿namespace ETicketSystem.Test.Mocks
+{
+	using Microsoft.AspNetCore.Mvc.ViewFeatures;
+	using Moq;
+
+	public static class TempDataMock
+    {
+		public static Mock<ITempDataDictionary> New =>
+			new Mock<ITempDataDictionary>();
+    }
+}
